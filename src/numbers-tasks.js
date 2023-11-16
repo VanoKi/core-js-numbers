a3d734bf2fbca377a2fdde29208065c0c840c089
VanoKi <kirsvano@gmail.com>
2023-11-16 10:44:20 +0300

    getDistanceBetwwnPoints complete

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -52,7 +52,8 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+  // throw new Error('Not implemented');
+  // return ((value1 + value2) / 2).toFixed();
 }
 
 /**
@@ -70,8 +71,9 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  // throw new Error('Not implemented');
+  return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5;
 }
 
 /**
