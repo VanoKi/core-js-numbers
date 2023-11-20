80ba064109a965b7415e9fa2e37b1c46b54dc54f
VanoKi <kirsvano@gmail.com>
2023-11-20 13:05:28 +0300

    roundToPowerOfTen complete

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -182,8 +182,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  const m = 10 ** pow;
+  return Math.round(num / m) * m;
 }
 
 /**
